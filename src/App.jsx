ff582211a93f48e29da914544e8f5dd83f25df01
Shahnaz Maulidini Arisa <shahnazmaulidini@gmail.com>
2024-08-28 14:18:55 +0700

    add menubar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Menubar from "./components/Menubar";
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -6,8 +7,10 @@ function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar/>
+      <Menubar/>
       </div>
   )
 }
 
 export default App;
+
